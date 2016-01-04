71b0fcb4ac1bcb06f35101fe8c5a989d84047dae
Ricky Elrod <ricky@elrod.me>
2016-01-04 08:38:55 -0500

    Rework mapview-websocket to make it actually work.
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/KD8ZRC/Mapview/Utility/Websocket.hs
+++ src/KD8ZRC/Mapview/Utility/Websocket.hs
@@ -13,6 +13,7 @@
 module KD8ZRC.Mapview.Utility.Websocket where
 
 import Control.Concurrent
+import qualified Control.Concurrent.Chan as Chan
 import Control.Exception (finally)
 import Control.Monad
 import Control.Monad.IO.Class
@@ -23,21 +24,30 @@ import qualified Data.UUID.V4 as UUIDv4
 import KD8ZRC.Mapview.Types
 import qualified Network.WebSockets as WS
 
-type Client = (String, WS.Connection)
+type Client = (String, Chan.Chan T.Text)
 type ServerState = [Client]
 
 data WebsocketServer = WebsocketServer (MVar ServerState)
 
-initWebsocketServer :: String -> Int -> IO WebsocketServer
-initWebsocketServer host port = do
+-- | To make use of @mapview-websocket@ in your MapView application, start off
+-- by initializing a new 'Chan.Chan T.Text'. To do this, in your @main@'
+-- function, write @rawChan <- Chan.newChan@. This should get passed to your
+-- 'MapviewConfig' and sent to 'writeChanRaw' in your '_mvPacketLineCallback'
+-- list.
+--
+-- Now you can start a 'WebsocketServer'. Still in @main@, fork a
+-- 'WebsocketServer' by doing (something like)
+-- @_ <- forkIO $ initWebsocketServer rawChan "127.0.0.1" 8181@, changing the
+-- hostname and port number to bind to, as necessary.
+--
+-- As MapView updates @rawChan@ (via the 'writeChanRaw' callback),
+-- @mapview-websocket@ will see the updates and broadcast them to clients as
+-- soon as it can.
+initWebsocketServer :: Chan.Chan T.Text -> String -> Int -> IO ()
+initWebsocketServer chan host port = do
   m <- newMVar []
-  let s = WebsocketServer m
-  _ <- forkIO (startServer s host port)
-  return s
-
-startServer :: WebsocketServer -> String -> Int -> IO ()
-startServer (WebsocketServer st) host port =
-  WS.runServer host port $ application st
+  _ <- forkIO (broadcast chan m)
+  WS.runServer host port (application m)
 
 addClient :: Client -> ServerState -> ServerState
 addClient = (:)
@@ -45,33 +55,28 @@ addClient = (:)
 removeClient :: Client -> ServerState -> ServerState
 removeClient client = filter ((/= fst client) . fst)
 
-broadcast :: T.Text -> ServerState -> IO ()
-broadcast message clients = do
-  T.putStrLn message
-  forM_ (map snd clients) (`WS.sendTextData` message)
+broadcast :: Chan.Chan T.Text -> MVar ServerState -> IO ()
+broadcast msg' clients' = forever $ do
+  msg <- readChan msg'
+  clients <- readMVar clients'
+  T.putStrLn msg
+  sequence $ (flip Chan.writeChan msg . snd) `fmap` clients
 
 application :: MVar ServerState -> WS.ServerApp
 application st pending = do
   conn <- WS.acceptRequest pending
   uuid <- UUIDv4.nextRandom
+  chan <- Chan.newChan
   putStrLn $ "New client: " ++ UUID.toString uuid
-  let client = (UUID.toString uuid, conn)
-  flip finally (disconnect client) $ do
-    liftIO $ modifyMVar_ st $ \s -> do
-      let s' = addClient client s
-      WS.sendTextData (snd client) ("Welcome!" :: T.Text)
-      broadcast "someone joined" s'
-      return s'
-    return ()
+  let client = (UUID.toString uuid, chan)
+  liftIO $ modifyMVar_ st $ \s -> do
+    let s' = addClient client s
+    return s'
+  flip finally (disconnect client) $ forever $ do
+    msg <- readChan chan
+    WS.sendTextData conn msg
   where
     disconnect client = do
-      s <- modifyMVar st $ \s ->
+      modifyMVar st $ \s ->
         let s' = removeClient client s in return (s', s')
-      broadcast "someone disconnected" s
-
-broadcastRaw :: WebsocketServer -> PacketLineCallback t
-broadcastRaw (WebsocketServer w) =
-  PacketLineCallback (
-    \s -> do
-      clients <- liftIO $ readMVar w
-      liftIO (broadcast (T.pack s) clients))
+      putStrLn $ "Disconnect : " ++ fst client
